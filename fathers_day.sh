a588c4fdd5895f5464aa7e09a090a4bdf431fbaa
Jamesy-tech <jamesycreator@gmail.com>
2026-09-06 07:42:09 +1200

    Add Father's Day greeting script

---
+++ fathers_day.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat << 'EOF'
+========================================
+
+       Happy Father's Day!!
+
+          /\_/\
+         ( o.o )
+          > ^ <
+
+     Thanks for everything you do!
+
+========================================
+EOF
