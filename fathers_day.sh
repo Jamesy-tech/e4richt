9a20dcbe9e6a4e51a39a27badb7eddb3807ed568
Jamesy-tech <jamesycreator@gmail.com>
2026-09-06 08:16:11 +1200

    Update fathers_day.sh

--- fathers_day.sh
+++ fathers_day.sh
@@ -1,73 +1,87 @@
 #!/bin/bash
 
+TITLE="Happy Father's Day!"
+MESSAGE="Thank you for everything you've done"
+MESSAGE3="I really appreciate everything you do"
+THANKS="Thank you, Dad!!"
+ENDING="I hope you have a great day!"
+FROM="From James (coding is life)"
+EXTRA="..."
+
 clear
 
-cols=$(tput cols)
-confetti_rows=5
-amount=15
+WIDTH=62
 
-for ((i=0; i<amount; i++)); do
-    x[$i]=$((RANDOM % cols + 1))
-    y[$i]=$((RANDOM % confetti_rows + 1))
-done
+type_text() {
+    text="$1"
+    delay="$2"
 
-printf '\n'
-printf '        *        .       *       .        *\n'
-printf '   .       *   .    *       .    *   .       *\n'
-printf '       *       Happy Father'"'"'s Day!       *\n'
-printf '   *       .       *       .       *       .\n'
-printf '\n'
-
-sleep 0.5
-
-cat << 'EOF'
-
-==============================================================
-                    Happy Father's Day!
-==============================================================
-
-                         /\_/\
-                        ( o.o )
-                         > ^ <
-
-           Thank you for everything you've done
-
-
-
-==============================================================
-
-                  Thank you, Dad!!
-
-==============================================================
-
-                 Have an awesome day!
-
-==============================================================
-
-EOF
-
-while true; do
-    for ((i=0; i<amount; i++)); do
-        printf "\033[${y[$i]};${x[$i]}H "
-
-        y[$i]=$((y[$i] + 1))
-
-        if [ ${y[$i]} -gt $confetti_rows ]; then
-            y[$i]=1
-            x[$i]=$((RANDOM % cols + 1))
-        fi
-
-        printf "\033[${y[$i]};${x[$i]}H"
-
-        case $((i % 6)) in
-            0) printf "\033[31m*\033[0m" ;;
-            1) printf "\033[33m.\033[0m" ;;
-            2) printf "\033[32m+\033[0m" ;;
-            3) printf "\033[34mo\033[0m" ;;
-            4) printf "\033[35mx\033[0m" ;;
-            5) printf "\033[36m#\033[0m" ;;
-        esac
+    for ((i=0; i<${#text}; i++)); do
+        printf '%s' "${text:$i:1}"
+        sleep "$delay"
     done
 
-    sleep 0.15
-done
+    printf '\n'
+}
+
+center_text() {
+    text="$1"
+    padding=$(( (WIDTH - ${#text}) / 2 ))
+    printf '%*s%s\n' "$padding" "" "$text"
+}
+
+border() {
+    printf '%*s\n' "$WIDTH" '' | tr ' ' '='
+}
+
+printf '\n'
+
+center_text '*        .       *       .        *'
+center_text '.       *   .    *       .    *   .       .'
+center_text "$TITLE"
+center_text '*       .       *       .       *       .'
+
+sleep 0.2
+
+printf '\n'
+
+type_text "$(border)" 0.002
+type_text "$(center_text "$TITLE")" 0.012
+type_text "$(border)" 0.002
+
+printf '\n'
+
+type_text "$(center_text '/\_/\\')" 0.008
+type_text "$(center_text '( o.o )')" 0.008
+type_text "$(center_text '> ^ <')" 0.008
+
+printf '\n'
+
+type_text "$(center_text "$MESSAGE")" 0.012
+type_text "$(center_text "$MESSAGE3")" 0.012
+
+printf '\n'
+
+type_text "$(border)" 0.002
+type_text "$(center_text "$THANKS")" 0.015
+type_text "$(border)" 0.002
+
+printf '\n'
+
+type_text "$(center_text "$EXTRA")" 0.012
+
+printf '\n'
+
+type_text "$(border | tr '=' '-')" 0.002
+type_text "$(center_text "$FROM")" 0.02
+type_text "$(border | tr '=' '-')" 0.002
+
+printf '\n'
+
+type_text "$(center_text "$ENDING")" 0.012
+
+printf '\n'
+
+type_text "$(border)" 0.002
+
+printf '\n'
